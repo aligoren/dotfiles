4b83d5d7522d5ba950a34b521242f364ebe4843d
Ali GOREN <goren.ali@yandex.com>
2016-04-15 23:35:17 +0300

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,20 @@
+import os
+ 
+rootDir = '.'
+for dirName, subdirList, fileList in os.walk(rootDir):
+    if dirName == ".":
+    	print('+-------%s' % "Current")
+    else:
+    	print('+-------%s' % dirName[2:])
+    print('\t|')
+    print('\t|')
+    print('\t|')
+    for fname in fileList:
+        print('\t↳ ------------%s' % fname) # ↳ yerine + olabilir.
+    # Remove the first entry in the list of sub-directories
+    # if there are any sub-directories present
+    if len(subdirList) > 0:
+        del subdirList[0]
+        print('\t|')
+        print('\t|')
+        #print('\t|')
